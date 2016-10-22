7e33e1b69dff3fdb3307c4894a26fe0ff20061b7
Nathan Tung <nathan.tung@redfin.com>
2016-10-22 15:36:51 -0700

    Configure hot loading for localhost:8080/web-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
@@ -6,7 +7,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         loaders: ['babel?presets[]=es2015,presets[]=react'],
-        exclude: /node_modules/
+        include: path.join(__dirname, 'src')
       },
       {
         test: /\.html$/,
@@ -15,11 +16,17 @@ module.exports = {
     ]
   },
   entry: [
-    __dirname + '/src/react/App.jsx',
-    __dirname + '/src/popup.html'
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, 'src/react/App.jsx'),
+    path.join(__dirname, 'src/popup.html')
   ],
   output: {
-    path: __dirname + '/dist',
-    filename: 'app.js'
-  }
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
